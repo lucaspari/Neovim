160769c12b6c405925c9bb9de6189970f6fde660
lucaspari <lucas.pari@icloud.com>
2024-02-01 14:31:42 -0300

    Add nvim-treesitter and tailwindcss LSP configurations

--- lua/lucaspari/lspconfig.lua
+++ lua/lucaspari/lspconfig.lua
@@ -1,26 +1,11 @@
 require("lspconfig").lua_ls.setup({
-	settings = {
-		Lua = {
-			runtime = {
-				-- Tell the language server which version of Lua you're using (most likely LuaJIT in the case of Neovim)
-				version = "LuaJIT",
-			},
-			diagnostics = {
-				-- Get the language server to recognize the `vim` global
-				globals = { "vim" },
-			},
-			workspace = {
-				-- Make the server aware of Neovim runtime files
-				library = vim.api.nvim_get_runtime_file("", true),
-			},
-			-- Do not send telemetry data containing a randomized but unique identifier
-			telemetry = {
-				enable = false,
-			},
-		},
-	},
+	filetypes = { "lua" },
 })
 require("lspconfig").tsserver.setup({})
+require("lspconfig").tailwindcss.setup({
+	filetypes = { "typescript", "javascript", "typescriptreact", "javascriptreact" },
+})
+
 -- Global mappings.
 -- See `:help vim.diagnostic.*` for documentation on any of the below functions
 vim.keymap.set("n", "<space>e", vim.diagnostic.open_float)
