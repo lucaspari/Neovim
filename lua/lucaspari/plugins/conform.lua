e0449cdbc3571cb85b5aa5a58caf83adcaef7bf5
lucaspari <lucas.pari@icloud.com>
2024-02-29 18:10:06 -0300

    disabled format on save

--- lua/lucaspari/plugins/conform.lua
+++ lua/lucaspari/plugins/conform.lua
@@ -7,30 +7,11 @@ return {
 			formatters_by_ft = {
 				lua = { "stylua" },
 				go = { "goimports", "gofumpt" },
-			},
-			format_on_save = {
-				lsp_fallback = true,
-				async = false,
-				timeout_ms = 500,
+				json = { "prettier" },
 			},
 		})
-		vim.keymap.set({ "n", "v" }, "<leader>fc", function()
-			conform.format({
-				lsp_fallback = true,
-				async = false,
-				timeout_ms = 500,
-			}, { desc = "Format File" })
-		end)
-		vim.api.nvim_create_user_command("Format", function(args)
-			local range = nil
-			if args.count ~= -1 then
-				local end_line = vim.api.nvim_buf_get_lines(0, args.line2 - 1, args.line2, true)[1]
-				range = {
-					start = { args.line1, 0 },
-					["end"] = { args.line2, end_line:len() },
-				}
-			end
-			require("conform").format({ async = true, lsp_fallback = true, range = range })
-		end, { range = true })
+		vim.keymap.set("", "<leader>fc", function()
+			require("conform").format({ async = true, lsp_fallback = true })
+		end, { desc = "[F]ormat" })
 	end,
 }
